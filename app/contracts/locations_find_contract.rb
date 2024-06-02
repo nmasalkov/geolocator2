478a072fc3b29e74b7a36cf2cd3df408a3360017
nmasalkov <nikitamasalkov.tang@gmail.com>
2024-06-02 18:43:19 +0200

    add pg for prod
    	modified:   config/database.yml

--- app/contracts/locations_find_contract.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class LocationsFindContract < Dry::Validation::Contract
-  params do
-    required(:source).filled(:string)
-    required(:type).filled(:string)
-  end
-end
